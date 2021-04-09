4790dfb2be76feb13f699bf201a8c2594ed31e2e
mustafasan088706 <san.mustafa0887@gmail.com>
2021-04-09 21:03:43 +0200

    ArrayList did carry to global area

--- src/newProject/ArabaDeneme.java
+++ src/newProject/ArabaDeneme.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArabaDeneme {
-
+	
+	static ArrayList<Araba> arabalar=new ArrayList<Araba>();
 	public static void main(String[] args) {
 		
-		ArrayList<Araba> arabalar=new ArrayList<Araba>();
+		
 		Scanner sc=new Scanner(System.in);
 		
 		
